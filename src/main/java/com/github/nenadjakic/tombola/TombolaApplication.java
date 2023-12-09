8a861739ca4e4a5cc8bc826f2a0e235b30b17f4d
nenadjakic <nenad.jakic@gmail.com>
2023-12-09 20:20:28 +0100

    Init project.

---
+++ src/main/java/com/github/nenadjakic/tombola/TombolaApplication.java
@@ -0,0 +1,12 @@
+package com.github.nenadjakic.tombola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TombolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TombolaApplication.class, args);
+	}
+}
